b095055d9e381e5e4fc2fc363b3706ac739c3141
石敬豪 <terryhappy510227@gmail.com>
2024-11-09 20:57:08 +0800

    更新 .gitignore，新增 README.md 以排除不必要的檔案；在 main.py 中新增錯誤處理機制，改善使用者輸入流程

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from iso_crawler import ISO_Crawler
 
 if __name__ == "__main__":
-    start = True
-    while start:
-        url = input("Enter the website URL(請輸入網站url): ")
-        if not url:
-            print("Please enter a valid URL(請輸入有效的網站url)")
-            continue
-        else:
-            start = False
-            output_path = input("Enter the output file path (輸入欲輸出文件名稱，default(預設值) : 'crawled_content.docx'): ")
-            if not output_path:
-                output_path = "crawled_content.docx"
-            ISO_Crawler(url, output_path)
+    try:
+        start = True
+        while start:
+            url = input("Enter the website URL(請輸入網站url): ")
+            if not url:
+                print("Please enter a valid URL(請輸入有效的網站url)")
+                continue
+            else:
+                start = False
+                output_path = input("Enter the output file path (輸入欲輸出文件名稱，default(預設值) : 'crawled_content.docx'): ")
+                if not output_path:
+                    output_path = "crawled_content.docx"
+                ISO_Crawler(url, output_path)
+                input("\nProcess completed. Press Enter to exit.")
+    except Exception as e:
+                # Catch any exceptions and print the error message
+        print(f"\nAn error occurred: {e}")
+        print("Press Enter to exit after reviewing the error message.")
+        input()
